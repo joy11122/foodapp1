2be5d1e534d0e416dbace4ba3e3e874bf6109257
joy11122 <joyhaldar112233@gmail.com>
2024-08-19 00:03:01 -0700

    fggm

--- src/lib/Connect.js
+++ src/lib/Connect.js
@@ -1,1 +1,1 @@
-export const connectstr="mongodb+srv://joyhaldar112233:E7DalwmHNE0221Sl@cluster008.n563pbv.mongodb.net/Ecom?retryWrites=true&w=majority";
+export const connectstr="mongodb+srv://joyhaldar112233:df2DGGN3f2s9rAyD@cluster008.n563pbv.mongodb.net/Ecom?retryWrites=true&w=majority";
