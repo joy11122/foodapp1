9175920b08fde20f48629253eba61b7a5ff3d83f
joy11122 <joyhaldar112233@gmail.com>
2024-08-20 20:21:22 -0700

    done

--- src/lib/Connect.js
+++ src/lib/Connect.js
@@ -1,1 +1,1 @@
-export const connectstr="mongodb+srv://joyhaldar112233:df2DGGN3f2s9rAyD@cluster0.ammvs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+export const connectstr="mongodb+srv://joyhaldar112233:df2DGGN3f2s9rAyD@cluster0.ammvs.mongodb.net/Ecom?retryWrites=true&w=majority&appName=Cluster0";
